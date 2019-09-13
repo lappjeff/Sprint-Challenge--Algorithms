2858a761a057d34065afc4f3051c37f36e4036c6
Jeff Lapp <lappjeff20@gmail.com>
2019-09-13 13:36:18 -0400

    Added comments to function

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,19 +5,23 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 
 def count_th(word):
+    #tracker for amount of th's in string
     th_count = 0
 
+    #helper function to maintain main function state
     def handle_word(word):
         nonlocal th_count
         word_length = len(word)
 
+        #select first two chars in word and increment if they equal "th"
         if word[0:2] == "th":
             th_count += 1
 
+        #exit case
         if word_length == 0:
             return word
         else:
-            #remove first character for every iteration
+            #pass slice of string from index 1 on for every call
             handle_word(word[1:])
 
     handle_word(word)
